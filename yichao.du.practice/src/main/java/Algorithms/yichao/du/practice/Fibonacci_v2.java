a852dde47f9273c47ac087bf987d41c19e7414da
Damien <yichao.du.damien@gmail.com>
2015-08-26 11:52:36 +0800

    initialization

---
+++ yichao.du.practice/src/main/java/Algorithms/yichao/du/practice/Fibonacci_v2.java
@@ -0,0 +1,39 @@
+/**
+ * Yichao Du.
+ * Copyright (c) 2015 All Rights Reserved.
+ */
+package Algorithms.yichao.du.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author macbookpro
+ * @version $Id: Fibonacci_v2.java, v 0.1 2015年8月15日 下午8:11:49 macbookpro Exp $
+ */
+public class Fibonacci_v2 {
+
+	public static long F(int N) {
+		List repository = new ArrayList();
+		if (N == 0)
+			return 0;
+		if (N == 1)
+			return 1;
+		
+		// TODO
+		
+		
+		
+		return 0;
+	}
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
